9e48c8f9c2f0262cc790f884e367cdcf27305d3a
Pablo <hello@pablorevuelta.com>
2017-07-19 15:58:42 +1200

    removed stupid folder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,51 +5,23 @@ import * as THREE from 'three';
 import OrbitControls from 'three-orbit-controls';
 const orbitControls = OrbitControls(THREE);
 
-const Helpers = require('./util/helpers');
-const Util = require('./util/util');
+import Helpers from './util/helpers';
+import Util from './util/util';
 
 let scene,
     camera,
-    clock,
     renderer,
-    mixer,
-    mixers = [],
-    rocks = [],
     controls;
 
 function init () {
 
     scene = new THREE.Scene();
     // scene.add(new THREE.AxisHelper(50));
-    clock = new THREE.Clock();
 
     camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position.z = 100;
     camera.position.y = 20;
     camera.target = new THREE.Vector3( 0, 0, 0 );
-
-    for (let i = 0; i < ROCK_COUNT; i++) {
-
-        let position = [Util.randomInt(-BOUNDS, BOUNDS), Util.randomInt(-BOUNDS, BOUNDS), Util.randomInt(-BOUNDS, BOUNDS)];
-        let dist = new THREE.Vector3(...position).distanceTo(new THREE.Vector3(0,0,0));
-        let size = 24/dist;
-
-        let rock = Rock(size);
-
-        rock.mesh.position.set(...position);
-        // let wf = Helpers.wireframe(rock.geometry)
-        // wf.position.set(...position);
-        // scene.add(wf);
-        // scene.add(Helpers.normals(rock));
-
-        let mixer = new THREE.AnimationMixer(rock.mesh);
-        let animation = mixer.clipAction(rock.clip);
-        animation.setLoop( THREE.Loop );
-        animation.play();
-        rocks.push(rock);
-        mixers.push(mixer);
-        scene.add(rock.mesh);
-    }
 
     controls = new orbitControls( camera );
 
@@ -67,8 +39,6 @@ window.addEventListener( 'resize', function () {
 }, false );
 
 function animate () {
-    let delta = clock.getDelta();
-    mixers.forEach(m => m.update(delta));
     requestAnimationFrame(animate);
     render();
 }
